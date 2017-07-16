286ba8fbf93eaaa87671475fa98b27c42ef24a17
pooshanadevi <shanamaragatham@gmail.com>
2017-07-16 20:33:04 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,15 @@
+package example;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=22;
+		if(n%2==0)
+			System.out.println("It Is Even Number");
+		else
+			System.out.println("It Is Odd Number");
+
+	}
+
+}
