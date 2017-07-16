8ab8116bd529a7a9b79e1a839435ee6a188ee2ad
pooshanadevi <shanamaragatham@gmail.com>
2017-07-16 20:25:53 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+package example;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		int r,sum=0,temp;
+	int	n=454;
+	temp=n;
+	while(n>0){
+		r=n%10;
+		sum=(sum*10)+r;
+		n=n/10;
+	}
+if(temp==sum)
+	System.out.println("This is palindrome");
+else
+	System.out.println("This is not palindrome");
+	}
+
+}
